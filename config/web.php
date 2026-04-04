a55aac3a403a3e4ce050d2b4d5d8ac1419b42024
Wilmer Arambula <terabytesoftw@gmail.com>
2026-04-04 10:30:46 -0300

    Apply fixed Coderabbitai review.

--- config/web.php
+++ config/web.php
@@ -96,7 +96,7 @@ $config = [
         'mailer' => MailerInterface::class,
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '1',
+            'cookieValidationKey' => '',
             'parsers' => [
                 'application/json' => \yii\web\JsonParser::class,
             ],
